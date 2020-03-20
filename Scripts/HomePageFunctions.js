cadb4739dba9fa50e940d8844c6bab4db8ff3067
FalseCrystal <kakitleong8@gmail.com>
2020-03-20 15:41:07 +0200

    Added Images, Buttons, profile pages, and a theory article
    Changes i probably shouldve pushed earlier

---
+++ Scripts/HomePageFunctions.js
@@ -0,0 +1,9 @@
+function revealHobby(){
+
+    let images=document.querySelectorAll("#GallerySources img"); 
+
+    for(let i=0;i<=images.length;i++){
+
+        images[i].src = images[i].getAttribute('data-src');
+    }
+}
